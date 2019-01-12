5058d45d5a8eb220ff4fdc6b6ca7d06e987efa15
Mario Meissner <mr.rash.boy@gmail.com>
2019-01-12 18:36:51 +0100

    Aniade sistema de puntuacion

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class camera : MonoBehaviour
 {
+
+    private int count;
+    private int lastCount = 0;
+    public Text cornerText;
+    public Text menuText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        print("Setting counter text, last count: " + lastCount.ToString());
+        SetCounterText();
     }
 
     // Update is called once per frame
@@ -18,10 +27,19 @@ public class camera : MonoBehaviour
 
     void OnCollisionEnter(Collision collisionInfo)
     {
-	if(collisionInfo.collider.tag=="Terrain"){
-	    Application.LoadLevel(Application.loadedLevel);    
+        if(collisionInfo.collider.tag=="Terrain"){
+            Application.LoadLevel(Application.loadedLevel);
+            lastCount = count;
+            SetCounterText();
         } else if(collisionInfo.collider.tag=="PickUp"){
-	    this.transform.parent.GetComponent<VueloController>().speed *= 2; 
+            this.transform.parent.GetComponent<VueloController>().speed *= 2; 
+            count += 1;
+            SetCounterText();
         }
+    }
+    void SetCounterText(){
+        cornerText.text = "Points: " + count.ToString();
+        print(lastCount.ToString());
+        menuText.text = "Points: " + lastCount.ToString();
     }
 }
