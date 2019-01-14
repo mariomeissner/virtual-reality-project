f9a796f6cdc66b19e494b7434ecfe5c848d22f71
Mario Meissner <mr.rash.boy@gmail.com>
2019-01-14 17:14:05 +0100

    Aniade plumas

--- Assets/pickup.cs
+++ Assets/pickup.cs
@@ -21,6 +21,20 @@ public class pickup : MonoBehaviour
     void OnCollisionEnter(Collision collisionInfo)
     {
         AudioSource.PlayClipAtPoint(pickupSound, transform.position);
-        Destroy(this.gameObject); 
+        
+        //generamos una posición aleatoria en una esfera envolvente de radio 1
+        Vector3 nuevaPosicion = Random.onUnitSphere;
+
+        //lo alejamos una distancia de nosotros
+        float distancia = 100 * Random.value + 3f;
+        nuevaPosicion *= distancia;
+
+        //restringimos la altura
+        //nuevaPosicion.y = Random.Range(0f, 1.5f);
+
+        //asignamos la posicion
+        transform.localPosition = nuevaPosicion;
+
+        Debug.Log("Interaccion");
     }
 }
